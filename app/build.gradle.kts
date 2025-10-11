aace4ab7206f24fe3f8e46416880ade343dc1c70
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-10-11 23:55:58 +0300

    add sonar, jacoco, checkstyle and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    id("application")
+    id("checkstyle")
+    id("jacoco")
     id("org.sonarqube") version "6.3.1.5724"
 }
 
@@ -11,17 +13,41 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("${rootDir}/config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
 }
 
 sonar {
     properties {
         property("sonar.projectKey", "pavelchervonenko_java-project-78")
         property("sonar.organization", "pavelchervonenko")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml").get().asFile.absolutePath
+        )
     }
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
